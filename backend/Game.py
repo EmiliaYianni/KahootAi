8261838cdbe47a303238cd9a60cc84110c3f2b4d
Emilia Yianni <Emilia.Yianni.2023@live.rhul.ac.uk>
2025-06-12 15:15:25 +0100

    Add Game class

---
+++ backend/Game.py
@@ -0,0 +1,12 @@
+
+
+
+class Game:
+    def __init__(self):
+        self.code = "xxxx"
+        self.players = []
+        self.questions = []
+
+    def addQuestions(question):
+        # add to self.questions
+        pass
--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,22 @@
-import random
+from flask import Flask, request, jsonify
+app = Flask(__name__)
+import json
+import Game
+
+games = []
+
+@app.route('/generateQuiz', methods=['POST'])
+def generateQuiz():
+    newGame = Game() 
+    # create questions
+    newGame.addQuestions()
+    games.append(newGame)
+    return newGame.code
+
+@app.route('/joinGame', methods=['POST'], params='gameCode')
+def joinGame(gameCode):
+    # search the games ARRAY for a matching game with this game code
+    pass
+
+if __name__ == "__main__":
+    app.run()
